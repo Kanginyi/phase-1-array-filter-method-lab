a5dcf40b439cb3051e566cc4cab6b080b3b4a937
Eric Yi <erickanginyi@gmail.com>
2021-09-03 14:12:09 -0400

    commit tree 674dcb564d1d0a228eb3cf2f6a203cf5ce9b9754

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solution here
+//const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+const findMatching = (array, string) => {
+    return array.filter(element => element.toLowerCase() === string.toLowerCase());
+}
+
+const fuzzyMatch = (array, string) => {
+  return array.filter(element => element.charAt(0) === string.charAt(0));
+}
+
+const matchName = (array, string) => {
+  return array.filter(element => element.name === string);
+}
